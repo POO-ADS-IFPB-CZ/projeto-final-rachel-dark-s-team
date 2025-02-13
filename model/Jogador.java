22b2503258104e6caeb3d880b255c14743bf96e8
Darkiane Lima <darkianelima13@gmail.com>
2025-02-13 16:51:02 -0300

    Segundo commit: Criando a class Jogador

---
+++ model/Jogador.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Jogador {
+    private String nome;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    public Jogador(String nome, int empates, int derrotas, int vitorias) {
+        this.nome = nome;
+        this.empates = 0;
+        this.derrotas = 0;
+        this.vitorias = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+}
